c3382245cce1fe26936ae39f8293d6a3c920d06c
Jarrod Lowe <jarrod@lowe.kiwi>
2026-04-11 20:31:14 +1200

    Dash Action

--- terraform/module/dnd-planner/dynamodb-items.tf
+++ terraform/module/dnd-planner/dynamodb-items.tf
@@ -550,6 +550,45 @@ resource "aws_dynamodb_table_item" "char_initiative_rulegroup_seed" {
   })
 }
 
+resource "aws_dynamodb_table_item" "char_dash_rulegroup_seed" {
+  table_name = aws_dynamodb_table.data.name
+  hash_key   = "PK"
+  range_key  = "SK"
+
+  item = jsonencode({
+    PK = {
+      S = "SEED#CHAR#$(characterId)"
+    }
+    SK = {
+      S = "RULEGROUP#dash"
+    }
+    gsiSeedPK = {
+      S = "SEED#CHAR"
+    }
+    type = {
+      S = "CHAR"
+    }
+    characterId = {
+      S = "$(characterId)"
+    }
+    ruleGroupId = {
+      S = "dash"
+    }
+    userId = {
+      S = "$(userId)"
+    }
+    enabled = {
+      BOOL = true
+    }
+    createdAt = {
+      S = "$(now)"
+    }
+    updatedAt = {
+      S = "$(now)"
+    }
+  })
+}
+
 # Species rule group seed - creates the species-specific rule group assignment
 resource "aws_dynamodb_table_item" "char_species_rulegroup_seed" {
   table_name = aws_dynamodb_table.data.name
