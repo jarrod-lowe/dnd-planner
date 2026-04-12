fe8e82f0d3c0cb87ac4d4689c530dfb59e498ce4
Jarrod Lowe <41766555+jarrod-lowe@users.noreply.github.com>
2026-04-12 16:19:15 +1200

    Heroic Inspiration (#186)

--- terraform/module/dnd-planner/dynamodb-items.tf
+++ terraform/module/dnd-planner/dynamodb-items.tf
@@ -589,6 +589,45 @@ resource "aws_dynamodb_table_item" "char_dash_rulegroup_seed" {
   })
 }
 
+resource "aws_dynamodb_table_item" "char_heroic_inspiration_rulegroup_seed" {
+  table_name = aws_dynamodb_table.data.name
+  hash_key   = "PK"
+  range_key  = "SK"
+
+  item = jsonencode({
+    PK = {
+      S = "SEED#CHAR#$(characterId)"
+    }
+    SK = {
+      S = "RULEGROUP#heroic-inspiration"
+    }
+    gsiSeedPK = {
+      S = "SEED#CHAR"
+    }
+    type = {
+      S = "CHAR"
+    }
+    characterId = {
+      S = "$(characterId)"
+    }
+    ruleGroupId = {
+      S = "heroic-inspiration"
+    }
+    userId = {
+      S = "$(userId)"
+    }
+    enabled = {
+      BOOL = true
+    }
+    createdAt = {
+      S = "$(now)"
+    }
+    updatedAt = {
+      S = "$(now)"
+    }
+  })
+}
+
 resource "aws_dynamodb_table_item" "char_simple_actions_rulegroup_seed" {
   table_name = aws_dynamodb_table.data.name
   hash_key   = "PK"
